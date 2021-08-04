9c34c8652e7794f310ec45a29c47af1c5fea1b7a
Yelizaveta <79942709+YelizavetaR@users.noreply.github.com>
2021-08-05 07:06:47 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div>
+      <WebViewer
+        initialDoc="https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import WebViewer from "./components/WebViewer.vue";
+
+export default {
+  name: "App",
+  components: {
+    WebViewer,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
